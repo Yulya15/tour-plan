0108289d2c3871786a0abcec067729e13279d4e4
Yulya15 <yasnikowa_yulyanka@mail.ru>
2021-03-16 23:39:19 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -61,3 +61,8 @@ function init(){
   myMap.geoObjects.add(placemark);
   myMap.behaviors.disable('scrollZoom'); 
 }
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function() {
+  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+});
